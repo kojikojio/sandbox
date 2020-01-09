2b90ed6f259581f81e1f29aa96d86d683f07124c
Shintaro Kojima <sh.kojima@connectom.co.jp>
2020-01-09 09:25:35 +0900

    wip for firebase auth

--- react-mobx/src/index.tsx
+++ react-mobx/src/index.tsx
@@ -7,11 +7,32 @@ import { Store } from "./stores/Store";
 import { StoreProvider } from "./stores/StoreHelper";
 import { CssBaseline } from "@material-ui/core";
 
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firebaseui = require("firebaseui");
+const firebaseConfig = {
+  apiKey: "AIzaSyCdJzFhZaJti6jY8FRWoSH5u7iSF9I0qKI",
+  authDomain: "sandbox-5d106.firebaseapp.com",
+  databaseURL: "https://sandbox-5d106.firebaseio.com",
+  projectId: "sandbox-5d106",
+  storageBucket: "sandbox-5d106.appspot.com",
+  messagingSenderId: "631189054380",
+  appId: "1:631189054380:web:35cbc6171d5200fe4dd009"
+};
+export const app = firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore(app);
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start("#firebaseui-auth-container", {
+  signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID]
+});
+
 const store = new Store();
 ReactDOM.render(
   <StoreProvider value={store}>
     <CssBaseline />
     <App />
+    <div id="firebaseui-auth-container"></div>
+    <div id="loader">Loading...</div>
   </StoreProvider>,
   document.getElementById("root")
 );
--- react-mobx/src/stores/TodoStore.ts
+++ react-mobx/src/stores/TodoStore.ts
@@ -1,17 +1,6 @@
 import { observable, computed, action } from "mobx";
-import firebase from "firebase/app";
 import "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyCdJzFhZaJti6jY8FRWoSH5u7iSF9I0qKI",
-  authDomain: "sandbox-5d106.firebaseapp.com",
-  databaseURL: "https://sandbox-5d106.firebaseio.com",
-  projectId: "sandbox-5d106",
-  storageBucket: "sandbox-5d106.appspot.com",
-  messagingSenderId: "631189054380",
-  appId: "1:631189054380:web:35cbc6171d5200fe4dd009"
-};
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore(app);
+import { db } from "../index";
 
 export class TodoStore {
   @observable todos: Todo[] = [];
