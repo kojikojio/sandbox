df4d16caa21e3377e476c81c2e9126d8ac340863
ShintaroKojima <shintaro.kojioma@gmail.com>
2020-05-31 22:25:28 +0900

    TypeScriptでgRPC

---
+++ ts-grpc/README.md
@@ -0,0 +1,2 @@
+gRPC を使ってみる
+https://adnanahmed.info/blog/2019/11/01/grpc-with-nodejs-typescript/
---
+++ ts-grpc/src/handlers/greeter.ts
@@ -0,0 +1,30 @@
+import * as grpc from "grpc";
+
+import { HelloRequest, HelloResponse } from "../proto/greeter/greeter_pb";
+import {
+  GreeterService,
+  IGreeterServer,
+} from "../proto/greeter/greeter_grpc_pb";
+
+class GreeterHandler implements IGreeterServer {
+  /**
+   * Greet the user nicely
+   * @param call
+   * @param callback
+   */
+  sayHello = (
+    call: grpc.ServerUnaryCall<HelloRequest>,
+    callback: grpc.sendUnaryData<HelloResponse>
+  ): void => {
+    const reply: HelloResponse = new HelloResponse();
+
+    reply.setMessage(`Hello, ${call.request.getName()}`);
+
+    callback(null, reply);
+  };
+}
+
+export default {
+  service: GreeterService, // Service interface
+  handler: new GreeterHandler(), // Service interface definitions
+};
