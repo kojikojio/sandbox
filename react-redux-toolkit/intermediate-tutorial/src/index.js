e1a4c10425dfb8d3f90156598a92ee64826c94c4
ShintaroKojima <shintaro.kojioma@gmail.com>
2020-06-03 08:36:43 +0900

    fix

--- react-redux-toolkit/intermediate-tutorial/src/index.js
+++ react-redux-toolkit/intermediate-tutorial/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { configureStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
